bb1d6d0f46f663b5e0aea731fbc5b504779b4b37
Charles <ystanglei@gmail.com>
2020-12-08 15:40:40 +0800

    image

--- Sources/MyLibrary/MyViewController.swift
+++ Sources/MyLibrary/MyViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 public class MyViewController: UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
-        let img = UIImageView(image: UIImage(named: "newnew_version_icon", in: Bundle.init(for: MyViewController.self), compatibleWith: nil))
+        let img = UIImageView(image: UIImage(named: "newnew_version_icon"))
         img.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
         view.addSubview(img)
     }
